ae0e5db1b959419729a88ec3c9910d1b893c88fb
Andrzej Kurek <andrzej.kurek@arm.com>
2019-06-11 12:28:52 +0200

    Add writing and reading raw data into/from a slot

--- atecc608a_se.c
+++ atecc608a_se.c
@@ -367,6 +367,42 @@ exit:
     return status;
 }
 
+psa_status_t atecc608a_write(uint16_t slot, size_t offset, const uint8_t *data, size_t length)
+{
+    psa_status_t status = PSA_ERROR_GENERIC_ERROR;
+
+    /* The hardware has slots 0-15 */
+    if (slot > 15)
+    {
+        return PSA_ERROR_INVALID_ARGUMENT;
+    }
+
+    ASSERT_SUCCESS_PSA(atecc608a_init());
+    ASSERT_SUCCESS(atcab_write_bytes_zone(ATCA_ZONE_DATA, slot, offset, data, length));
+
+exit:
+    atecc608a_deinit();
+    return status;
+}
+
+psa_status_t atecc608a_read(uint16_t slot, size_t offset, uint8_t *data, size_t length)
+{
+    psa_status_t status = PSA_ERROR_GENERIC_ERROR;
+
+    /* The hardware has slots 0-15 */
+    if (slot > 15)
+    {
+        return PSA_ERROR_INVALID_ARGUMENT;
+    }
+
+    ASSERT_SUCCESS_PSA(atecc608a_init());
+    ASSERT_SUCCESS(atcab_read_bytes_zone(ATCA_ZONE_DATA, slot, offset, data, length));
+
+exit:
+    atecc608a_deinit();
+    return status;
+}
+
 #define PSA_ATECC608A_LIFETIME 0xdeadbeefU
 
 static psa_drv_se_asymmetric_t atecc608a_asymmetric =
